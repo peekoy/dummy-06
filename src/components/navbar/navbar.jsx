efa81337c6afd9ff45954345849b03cc6ae9e8ff
pekoy <mhmmdfaiz160@gmail.com>
2023-06-04 02:04:20 +0700

    update files

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return(
+    <div className="navbar">
+      <h2>Rentalls</h2>
+      <ul>
+        <li><a href="#"></a>Locations</li>
+        <li><a href="#"></a>Cars</li>
+        <li><a href="#"></a>My Booking</li>
+        <li><a href="#"></a>Contact</li>
+        <li><a href="#"></a>IDR</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar;
