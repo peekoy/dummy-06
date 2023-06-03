d4cd830dadd2530971a1814008c261580e57c1ff
pekoy <mhmmdfaiz160@gmail.com>
2023-06-04 04:21:50 +0700

    Update: files

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Navbar from './navbar/navbar'
+import Banner from './banner/banner'
 import './css/main.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar />
+    <Banner />
   </React.StrictMode>,
 )
