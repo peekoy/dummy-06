e8dee83a5571539435e8f6c17ed2569b029683b1
pekoy <mhmmdfaiz160@gmail.com>
2023-06-04 02:40:42 +0700

    add banner

---
+++ src/components/banner/banner.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <h2>What's on your mind?</h2>
+      <div className="flex-container-01">
+        <h5>Drop car off at the different location</h5>
+      </div>
+    </div>
+
+  )
+  
+
+}
+
+export default Banner;
