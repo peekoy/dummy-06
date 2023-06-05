e98f0d14a253009ca7303e6ad98cdeb740fdcecc
pekoy <mhmmdfaiz160@gmail.com>
2023-06-06 00:06:11 +0700

    Update: banner

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -21,25 +21,41 @@ const Banner = () => {
           </div>
           <div className="container-02">
             <div className="sub-container">
+              <div className="content-02">
                 <p>Pick-up date</p>
+              </div>
+              <div className="content-03">
                 <p>Wed 10 Oct</p>
+              </div>
             </div>
             <div className="sub-container">
-              <div className="content">
-                <p>Time</p>
-                <p>11:59 AM</p>
+              <div className="border">
+                <div className="content-02">
+                  <p>Time</p>
+                </div>
+                <div className="content-03">
+                  <p>11:59 AM</p>
+                </div>
               </div>
             </div>
           </div>
           <div className="container-02">
             <div className="sub-container">
-              <p>Drop-off date</p>
-              <p>Wed 10 Oct</p>
+              <div className="content-02">
+                <p>Drop-off date</p>
+              </div>
+              <div className="content-03">
+                <p>Wed 10 Oct</p>
+              </div>
             </div>
             <div className="sub-container">
-              <div className="content">
-                <p>Time</p>
-                <p>11:59 PM</p>
+              <div className="border">
+                <div className="content-02">
+                  <p>Time</p>
+                </div>
+                <div className="content-03">
+                  <p>11:59 PM</p>
+                </div>
               </div>
             </div>
           </div>
