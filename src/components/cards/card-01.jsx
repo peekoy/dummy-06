cfac6888bd357760ba729f17619538d19f53459c
pekoy <mhmmdfaiz160@gmail.com>
2023-06-07 01:59:46 +0700

    Update: content

--- src/components/cards/card-01.jsx
+++ src/components/cards/card-01.jsx
@@ -9,12 +9,36 @@ const Card_01 = (props) => {
         </div>
       </div>
         <div className="content">
+          <div className="container-04">
           <img src={props.img} />
-          <p>{props.cars}</p>
-          <p>{props.type}</p>
-          <p>{props.seats}</p>
-          <p>{props.price1}</p>
-          <p>{props.price2}</p>  
+            <div className="cars">
+              <p>{props.cars}</p>
+            </div>
+            <div className="container-05">
+              {/* <div className="gap"> */}
+                <div className="icon-02">
+                  <img src={props.icon1} />
+                </div>
+                <div className="type">
+                  <p>{props.type}</p>
+                </div>
+                <div className="icon-03">
+                  <img src={props.icon2} />
+                </div>
+                <div className="seats">
+                  <p>{props.seats}</p>
+                </div>
+              </div>
+            {/* </div> */}
+            <div className="container-06">
+              <div className="price-01">
+                <p>{props.price1}</p>
+              </div>
+              <div className="price-02">
+                <p>{props.price2}</p>  
+              </div>
+            </div>
+          </div>
         </div>
     </div>
   )
--- src/components/content/content.jsx
+++ src/components/content/content.jsx
@@ -3,11 +3,12 @@ import Card_01 from "../cards/card-01";
 import Sigra from "../../assets/sigra.png";
 import HRV from "../../assets/HR-V.png";
 import Alphard from "../../assets/alphard.png";
+import Transmission from "../../assets/transmission.png"
+import Seat from "../../assets/seat.png" 
 
 const Content = () => {
   return (
     <div className="main-container-02">
-
       <div className="header">
         <h3>Today deals</h3>
       </div>
@@ -15,9 +16,9 @@ const Content = () => {
         <p>Get the cheapest car rental price rate from us!</p>
       </div>
       <div className="flex-container-02">
-        <Card_01 deals="Save 10%" img={Sigra} cars="Daihatsu Sigra" type="Automatic" seats="7 Seats" price1="Rp385.000" price2="Rp350.000" />
-        <Card_01 deals="Save 10%" img={HRV} cars="Daihatsu Sigra" type="Automatic" seats="7 Seats" price1="Rp385.000" price2="Rp350.000" />
-        <Card_01 deals="Save 10%" img={Alphard} cars="Daihatsu Sigra" type="Automatic" seats="7 Seats" price1="Rp385.000" price2="Rp350.000" />
+        <Card_01 deals="Save 10%" img={Sigra} cars="Daihatsu Sigra" icon1={Transmission} type="Automatic" icon2={Seat} seats="7 Seats" price1="Rp385.000" price2="Rp350.000/day" />
+        <Card_01 deals="Save 15%" img={HRV} cars="Honda HR-V" icon1={Transmission} type="Automatic" icon2={Seat} seats="5 Seats" price1="Rp620.000" price2="Rp500.000/day" />
+        <Card_01 deals="Save 5%" img={Alphard} cars="Toyota Alphard" icon1={Transmission} type="Automatic" icon2={Seat} seats="6 Seats" price1="Rp2.200.000" price2="Rp2.090.000/day" />
       </div>
     </div>
 
