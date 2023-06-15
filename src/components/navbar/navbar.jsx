88a889721de811747b72f83c24890ada4e6fec1e
pekoy <mhmmdfaiz160@gmail.com>
2023-06-15 20:56:58 +0700

    Chores: fonts

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import Flag from "../../assets/flag.svg"
+import Down from "../../assets/down.png"
 
 const Navbar = () => {
   return(
     <div className="navbar">
       <h2>Rentalls</h2>
-      <ul>
-        <li><a href="#"></a>Locations</li>
-        <li><a href="#"></a>Cars</li>
-        <li><a href="#"></a>My Booking</li>
-        <li><a href="#"></a>Contact</li>
-        <li><a href="#"></a>IDR</li>
-      </ul>
+        <ul>
+          <li><a href="#"></a>Locations</li>
+          <li><a href="#"></a>Cars</li>
+          <li><a href="#"></a>My Booking</li>
+          <li><a href="#"></a>Contact</li>
+          <li><a href="#"></a>IDR</li>
+        </ul>
     </div>
   )
 }
